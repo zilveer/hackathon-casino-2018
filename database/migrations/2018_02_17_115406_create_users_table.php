5fa01fae370d6e7da1eb3374572a3f69dd48dc62
Benjamin Delespierre <benjamin.delespierre@gmail.com>
2018-02-17 13:46:30 +0100

    fix migration name

--- database/migrations/2018_02_17_115406_create_users_table.php
+++ database/migrations/2018_02_17_115406_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomersTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
